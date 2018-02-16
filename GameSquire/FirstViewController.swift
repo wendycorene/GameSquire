380dde7d6012709aec45562ad4ee67084ccfd164
Eloe,Wendy C <s524779@ch140029m.nwmissouri.edu>
2018-02-16 17:03:03 -0600

    Initial Commit

---
+++ GameSquire/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GameSquire
+//
+//  Created by Eloe,Wendy C on 2/16/18.
+//  Copyright © 2018 Eloe,Wendy C. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
