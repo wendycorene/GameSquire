76abea405d6c9464c227ee9292b2f8e84fc76d7d
Eloe,Wendy C <s524779@ch140029m.nwmissouri.edu>
2018-03-07 17:06:16 -0600

    Add basic table view for items
    I added one single item for a random shop generator.

--- GameSquire/AppDelegate.swift
+++ GameSquire/AppDelegate.swift
@@ -12,10 +12,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    static var myModel: Shop!
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        AppDelegate.myModel = Shop()
+        AppDelegate.myModel.loadBasicShop()
         return true
     }
 
---
+++ GameSquire/ShopModel.swift
@@ -0,0 +1,30 @@
+//
+//  ShopModel.swift
+//  GameSquire
+//
+//  Created by Eloe,Wendy C on 3/7/18.
+//  Copyright © 2018 Eloe,Wendy C. All rights reserved.
+//
+
+import Foundation
+
+struct ShopItem {
+    var name: String
+    var price: Int
+    var description: String
+    var quantity: Int
+    var weight: Double
+}
+
+class Shop {
+    var items: [ShopItem]
+    
+    init () {
+        self.items = []
+    }
+    
+    func loadBasicShop(){
+        let healingPotion = ShopItem(name: "Potion of Healing", price: 50, description: "You regain 2d4 + 2 hit points when you drink this potion. The potion's red liquid glimmers when agitated.", quantity: 1, weight: 0.5)
+        items.append(healingPotion)
+    }
+}
