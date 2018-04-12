88f6de0f04b937b1a9c33c9b511a6ad4885c1654
Root,Hunter D <s524167@ch140027m.nwmissouri.edu>
2018-04-12 18:55:19 -0500

    has changed the background of the create shop and load shop, along with more font corrections.

---
+++ GameSquire/loadShopViewController.swift
@@ -0,0 +1,36 @@
+//
+//  loadShopViewController.swift
+//  GameSquire
+//
+//  Created by Root,Hunter D on 4/12/18.
+//  Copyright © 2018 Eloe,Wendy C. All rights reserved.
+//
+
+import UIKit
+
+class loadShopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //This should change the background color - Hunter
+        self.view.backgroundColor = UIColor(red: 214/255, green: 204/255, blue: 169/255, alpha: 1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
