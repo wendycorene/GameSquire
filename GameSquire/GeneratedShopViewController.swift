4389e1ae92cd71f779f1dc2a71290540fd4119d0
Eloe,Wendy C <s524779@ch140029m.nwmissouri.edu>
2018-04-18 15:35:07 -0500

    generate new shop button now does it

--- GameSquire/GeneratedShopViewController.swift
+++ GameSquire/GeneratedShopViewController.swift
@@ -13,20 +13,9 @@ class GeneratedShopViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        NotificationCenter.default.addObserver(self, selector: #selector(updated_data), name:Notification.Name("UPDATED_DATA"), object: nil)
-
-        // Do any additional setup after loading the view.
         
         //This should change the background color - Hunter
         self.view.backgroundColor = UIColor(red: 214/255, green: 204/255, blue: 169/255, alpha: 1)
-    }
-
-    override func viewDidDisappear(_ animated: Bool) {
-        NotificationCenter.default.removeObserver(self)
-    }
-    
-    @objc func updated_data(notification:Notification) -> Void{
-        itemDesc.text = AppDelegate.myModel.itemDesc
     }
     
     @IBOutlet weak var itemDesc: UITextView!
@@ -58,6 +47,18 @@ class GeneratedShopViewController: UIViewController {
 
     func unwindFromAlert(segue:UIStoryboardSegue) {
     
+    }
+    
+    @IBAction func generateNewShopBTN(_ sender: UIButton) {
+        
+        AppDelegate.myModel.items = []
+        AppDelegate.myModel.randomizeShop()
+        let alert = UIAlertController(title: "Generate New Shop", message: "Do you want to generate a different shop?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            NotificationCenter.default.post(name: Notification.Name("UPDATED_DATA"), object: nil)
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
     }
     
     @IBAction func clearShopBTN(_ sender: UIButton) {
